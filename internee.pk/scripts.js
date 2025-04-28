79fd796daa58622749ca023a7feeae84018e66b7
abdalkhaild <abdalkhalid11@gmail.com>
2025-04-28 00:25:28 -0700

    Add codecrafts project, internee.pk files, portfolio, and web-basics content

---
+++ internee.pk/scripts.js
@@ -0,0 +1,17 @@
+
+  tailwind.config = {
+    theme: {
+      extend: {
+        fontFamily: {
+          poppins: ["Poppins", "sans-serif"],
+        },
+      },
+    },
+  };
+
+  var typed = new Typed('#typed-text', {
+    strings: ["dream internship?", "career opportunities?", "professional growth?"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+  });

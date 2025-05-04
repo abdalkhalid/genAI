1373949d4a32258f7c49846fcb3dcd5ddfbab793
abdalkhaild <abdalkhalid11@gmail.com>
2025-05-04 14:05:11 -0700

    Merge portfolio

--- portfolio/scripts.js
+++ portfolio/scripts.js
@@ -3,5 +3,8 @@ const themeToggle = document.getElementById('theme-toggle');
 themeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
- 
-/* <link rel="shortcut icon" href="https://cdn-icons-png.freepik.com/256/8931/8931836.png?ga=GA1.1.1918459279.1734936979&semt=ais_hybrid" type="image/x-icon"> */}
+
+// Handle smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
